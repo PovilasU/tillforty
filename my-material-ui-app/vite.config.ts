1be47c0e694e8e9dd2e239a15fac7aa2149ff513
PovilasU <povilas.urbonas85@gmail.com>
2025-01-31 21:14:53 +0200

    inital commit

---
+++ my-material-ui-app/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  esbuild: {
+    loader: "tsx",
+    include: [/src\/.*\.[tj]sx?$/],
+  },
+});
