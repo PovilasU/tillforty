11e6388ad0b5e46a87987ccea4489720fa730f77
PovilasU <povilas.urbonas85@gmail.com>
2025-02-02 22:18:13 +0200

    printing  newtwork ip address, update front end patch to serve

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
+const os = require('os');
 const pool = require('./db/dbConfig');
 const authRoutes = require('./routes/authRoutes');
 
@@ -16,11 +17,24 @@ app.use(cors({
 }));
 app.use(bodyParser.json());
 
-// Serve static files from the /dist directory
-app.use(express.static(path.join(__dirname, 'dist')));
+// Serve static files from the ../my-material-dashboard/dist directory
+app.use(express.static(path.join(__dirname, '../my-material-ui-app/dist')));
 
 // Routes
 app.use('/api/auth', authRoutes(pool));
+
+// Function to get the local network IP address
+const getLocalNetworkIP = () => {
+  const interfaces = os.networkInterfaces();
+  for (const name of Object.keys(interfaces)) {
+    for (const iface of interfaces[name]) {
+      if (iface.family === 'IPv4' && !iface.internal) {
+        return iface.address;
+      }
+    }
+  }
+  return 'localhost';
+};
 
 // Test database connection
 pool.connect((err, client, release) => {
@@ -32,6 +46,8 @@ pool.connect((err, client, release) => {
 
   // Start the server
   app.listen(port, () => {
+    const localNetworkIP = getLocalNetworkIP();
     console.log(`Server running on http://localhost:${port}`);
+    console.log(`Server running on http://${localNetworkIP}:${port}`);
   });
 });
